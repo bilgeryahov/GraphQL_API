9c944383e24ddd48b370c17dd41f75827f8d6cec
Bilger Yahov <bayahov1@gmail.com>
2018-03-02 12:54:55 +0100

    Create Schema for handling queries and mutations.

---
+++ GraphQL_API/src/schema/index.js
@@ -0,0 +1,31 @@
+import posts from './queries/posts';
+import post from './queries/post';
+import author from './queries/author';
+
+import addPost from './mutations/addPost';
+import addComment from './mutations/addComment';
+
+import {
+	GraphQLSchema,
+	GraphQLObjectType
+}
+from 'graphql';
+
+const schema = new GraphQLSchema({
+	query: new GraphQLObjectType({
+		name: 'RootQuery',
+		fields: () => ({
+			...author,
+			...post,
+			...posts
+		})
+
+	}),
+	mutation: new GraphQLObjectType({
+		name: 'RootMutation',
+		fields: () => ({
+			...addComment,
+			...addPost
+		})
+	})
+});
