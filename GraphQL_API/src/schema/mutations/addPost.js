52a1f8a7fd4bb9437fa71a6673e0fc6b5ffcb02f
Bilger Yahov <bayahov1@gmail.com>
2018-03-02 12:48:38 +0100

    Implement mutations, which will be used to submit new data.

---
+++ GraphQL_API/src/schema/mutations/addPost.js
@@ -0,0 +1,21 @@
+import { fakeDatabase } from '../../FakeDatabase';
+
+import {
+	Post,
+	PostInputType
+} from '../types/Post';
+
+export default {
+	addPost: {
+		type: Post,
+		description: 'Creates a new blog post',
+		args: {
+			post: {
+				type: PostInputType
+			}
+		},
+		resolve: (parent, { post }) => {
+			return fakeDatabase.addNewBlogPost(post);
+		}
+	}
+}
